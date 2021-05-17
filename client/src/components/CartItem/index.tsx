efaff9e6cd4e0bd4b2efa14ebe2b4a8c0270831d
A. Monae Payne <amonaepayne@gmail.com>
2021-05-17 05:22:49 -0500

    :wheelchair: add alt tag to cart item image

--- client/src/components/CartItem/index.tsx
+++ client/src/components/CartItem/index.tsx
@@ -45,7 +45,7 @@ export default function ({ id, title, quantity }: CartItemProps) {
         <Img
           src="https://images.unsplash.com/photo-1534308983496-4fabb1a015ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1355&q=80"
           height="80"
-          alt="image"
+          alt="peperoni pizza"
         />
       </ImgContainer>
       <Info>
