bbf16cd6e453fcc157d648594431393b9b0352ad
A. Monae Payne <amonaepayne@gmail.com>
2021-05-18 06:52:48 -0500

    :children_crossing::lipstick: create loading/spinner component
    This improves UX significantly! Now instead of the page just being
    void of any data/cards and the user assuming something went wrong/that
    the site is broken, the spinner component will indicate to the user
    "don't worry, the data is loading!"

---
+++ client/src/components/Spinner/SpinnerStyles.ts
@@ -0,0 +1,44 @@
+import styled, { keyframes } from "styled-components";
+
+const spin = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+export const SpinnerElement = styled.div`
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+  margin: 0 auto;
+  margin-top: 90%;
+
+  div {
+    box-sizing: border-box;
+    display: block;
+    position: absolute;
+    width: 64px;
+    height: 64px;
+    margin: 8px;
+    border: 8px solid #d72d2c;
+    border-radius: 50%;
+    animation: ${spin} 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    border-color: #d72d2c transparent transparent transparent;
+  }
+
+  div:nth-child(1) {
+    animation-delay: -0.45s;
+  }
+
+  div:nth-child(2) {
+    animation-delay: -0.3s;
+  }
+
+  div:nth-child(3) {
+    animation-delay: -0.15s;
+  }
+`;
---
+++ client/src/components/Spinner/index.tsx
@@ -0,0 +1,12 @@
+import { SpinnerElement } from "./SpinnerStyles";
+
+export default function Spinner() {
+  return (
+    <SpinnerElement>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </SpinnerElement>
+  );
+}
